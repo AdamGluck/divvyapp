428acda6274e3f6e3eccccb61a3d513fa3b04eb7
Andrew <abeinstein@uchicago.edu>
2013-07-05 16:04:15 -0500

    Added map view (commented out)

--- DivvyApp/DivvyApp/ViewController.m
+++ DivvyApp/DivvyApp/ViewController.m
@@ -9,6 +9,7 @@
 #import "ViewController.h"
 #import "BGLDivvyDataAccess.h"
 #import <CoreLocation/CoreLocation.h>
+#import <GoogleMaps/GoogleMaps.h>
 
 @interface ViewController () <BGLDivvyDataAccessDelegate>{
     int count;
@@ -16,6 +17,7 @@
 
 @property (strong, nonatomic) BGLDivvyDataAccess * dataAccess;
 @property (strong, nonatomic) IBOutlet UITextField *nearestLocationText;
+//@property (strong, nonatomic) GMSMapView *mapView_;
 
 @end
 
@@ -32,8 +34,22 @@
     
     // "latitude":41.8739580629,"longitude":-87.6277394859 should be the station on State St & Harrison St
     // for testing
+    //[self loadView];
     
 }
+
+//- (void)loadView
+//{
+//    GMSCameraPosition *camera = [GMSCameraPosition cameraWithLatitude:41.8739580629 longitude:-87.6277394859 zoom:6];
+//    self.mapView_ = [GMSMapView mapWithFrame:CGRectZero camera:camera];
+//    self.mapView_.myLocationEnabled = YES;
+//    self.view = self.mapView_;
+//    
+//    GMSMarker *marker = [[GMSMarker alloc] init];
+//    marker.position = CLLocationCoordinate2DMake(41.8739580629, -87.6277394859);
+//    marker.title = @"State St and Harrison St";
+//    marker.map = self.mapView_;
+//}
 
 -(void) asynchronousFillRequestComplete: (NSArray *) data{
     NSLog(@"this means that your fill request is complete");
