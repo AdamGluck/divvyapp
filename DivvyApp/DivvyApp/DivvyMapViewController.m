1d491f668398bfddf39ec3597a67df8d89ed9ed5
AdamGluck <adamt.gluck@gmail.com>
2013-07-08 11:07:41 -0500

    changes

--- DivvyApp/DivvyApp/DivvyMapViewController.m
+++ DivvyApp/DivvyApp/DivvyMapViewController.m
@@ -108,38 +108,21 @@
 }
 
 -(void) nearestStationToDeviceFoundWithStation: (BGLStationObject *) station{
-    
-    /*
-    NSLog(@"station found == %@", station.stationName);
-    
-    
+        
     NSString *positionString1 = [[NSString alloc] initWithFormat:@"%f,%f", self.currentLocation.coordinate.latitude,self.currentLocation.coordinate.longitude];
     
     NSString *positionString2 = [[NSString alloc] initWithFormat:@"%f,%f", station.latitude,station.longitude];
     
-    NSString *positionString3 = @"41.8739580629,-87.6277394859";
 
     NSArray * waypoints = @[positionString1, positionString2];
-    NSLog(@"waypoints initial = %@", waypoints);
     GoogleBikeRoute * route = [[GoogleBikeRoute alloc] init];
     
     route.waypoints = [waypoints copy];
     route.appDoesUseGPS = YES;
     route.delegate = self;
     [route goWithTransportationType:kTransportationTypeBiking];
-    
-    GoogleBikeRoute * route2 = [[GoogleBikeRoute alloc] init];
-    route2.waypoints = [waypoints copy];
-    route2.appDoesUseGPS = YES;
-    route2.delegate = self;
-    
-    waypoints = @[positionString2, positionString3];
-
-    route2.waypoints = [waypoints copy];
-    
-    [route2 goWithTransportationType:kTransportationTypeWalking];
      
-    */
+
 }
 
 -(void) routeWithPolyline: (GMSPolyline *) polyline{
