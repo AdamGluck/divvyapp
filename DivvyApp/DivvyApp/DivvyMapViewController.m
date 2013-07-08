6eccbe0b9adceaa8e4490c01a2586c0979e3a867
Andrew <abeinstein@uchicago.edu>
2013-07-08 00:14:50 -0500

    Merged changes

--- DivvyApp/DivvyApp/DivvyMapViewController.m
+++ DivvyApp/DivvyApp/DivvyMapViewController.m
@@ -27,7 +27,16 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view.
+    self.dataAccess = [[BGLDivvyDataAccess alloc] init];
+    
+    self.dataAccess.delegate = self;
+    // for testing
+    // "latitude":41.8739580629,"longitude":-87.6277394859 should be the station on State St & Harrison St
+    
     [self loadMap];
+    
+    [self.dataAccess fillStationDataSynchronously];
+    [self.dataAccess grabNearestStationToDeviceWithOption:kNearestStationAny];
 }
 
 
@@ -65,5 +74,60 @@
     return _endLocation;
 }
 
+-(void) deviceLocationFoundAtLocation:(CLLocation *)deviceLocation{
+    self.currentLocation = deviceLocation;
+}
+
+-(void) nearestStationToDeviceFoundWithStation: (BGLStationObject *) station{
+    
+    /*
+    NSLog(@"station found == %@", station.stationName);
+    
+    
+    NSString *positionString1 = [[NSString alloc] initWithFormat:@"%f,%f", self.currentLocation.coordinate.latitude,self.currentLocation.coordinate.longitude];
+    
+    NSString *positionString2 = [[NSString alloc] initWithFormat:@"%f,%f", station.latitude,station.longitude];
+    
+    NSString *positionString3 = @"41.8739580629,-87.6277394859";
+
+    NSArray * waypoints = @[positionString1, positionString2];
+    NSLog(@"waypoints initial = %@", waypoints);
+    GoogleBikeRoute * route = [[GoogleBikeRoute alloc] init];
+    
+    route.waypoints = [waypoints copy];
+    route.appDoesUseGPS = YES;
+    route.delegate = self;
+    [route goWithTransportationType:kTransportationTypeBiking];
+    
+    GoogleBikeRoute * route2 = [[GoogleBikeRoute alloc] init];
+    route2.waypoints = [waypoints copy];
+    route2.appDoesUseGPS = YES;
+    route2.delegate = self;
+    
+    waypoints = @[positionString2, positionString3];
+
+    route2.waypoints = [waypoints copy];
+    
+    [route2 goWithTransportationType:kTransportationTypeWalking];
+     
+    */
+}
+
+-(void) routeWithPolyline: (GMSPolyline *) polyline{
+    polyline.map = mapView_;
+}
+
+
+-(void) directionsFromServer: (NSDictionary *) directionsDictionary{
+   // NSLog(@"directions = %@", directionsDictionary);
+}
+
+
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
 
 @end
